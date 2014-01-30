6f62c837091d076b8e8364f950281cc36ce92136
max-bakaev <maxkv333@gmail.com>
2014-01-31 00:57:02 +0200

    f

---
+++ app/assets/javascripts/app/test.js
@@ -0,0 +1,14 @@
+//= require app/jquery.tools.min
+//= require app/jquery.easing.1.3
+//= require app/jquery.tools.min
+//= require app/jquery.easing.1.3
+//= require app/jquery.carousel
+//= require app/superfish
+//= require app/jquery.bxslider
+//= require app/jquery.flexslider
+//= require app/tinynav
+//= require app/superfish
+//= require app/jquery.fancybox.pack
+//= require app/jquery.hoverdir
+//= require app/custom
+//= require app/jquery.cycle.lite
--- app/assets/javascripts/application_app_manifest.js
+++ app/assets/javascripts/application_app_manifest.js
@@ -1,17 +1,3 @@
 //= require jquery
 //= require jquery_ujs
-
-//= require app/jquery.tools.min
-//= require app/jquery.easing.1.3
-//= require app/jquery.tools.min
-//= require app/jquery.easing.1.3
-//= require app/jquery.carousel
-//= require app/superfish
-//= require app/jquery.bxslider
-//= require app/jquery.flexslider
-//= require app/tinynav
-//= require app/superfish
-//= require app/jquery.fancybox.pack
-//= require app/jquery.hoverdir
-//= require app/custom
-//= require app/jquery.cycle.lite
+//= require app/bootstrap
