adf7caebd7b72ff893864889bb27845ffa33e252
artem-kostenko <artem.kostenko@pokupon.ua>
2014-02-05 14:01:31 +0200

    s

--- app/mailers/server_error_mailer.rb
+++ app/mailers/server_error_mailer.rb
@@ -1,13 +1,13 @@
 class ServerErrorMailer < ActionMailer::Base
-  default :from => "Rails Cartender <rails.notifier@cartender.ua>"
+  default :from => "Rails Shopping.Farm <rails.notifier@cartender.ua>"
 
   def email_error(exception_o)
     @exception_o = exception_o
-    mail(:to => 'a@artt.com.ua', :subject => "Rails Cartender Error" )
+    mail(:to => 'a@artt.com.ua', :subject => "Rails Shopping.Farm Error" )
   end
 
   def email_js_error(exception_o)
     @exception_o = exception_o
-    mail(:to => 'a@artt.com.ua', :subject => "Rails Cartender Error" )
+    mail(:to => 'a@artt.com.ua', :subject => "Rails Shopping.Farm Error" )
   end
 end
